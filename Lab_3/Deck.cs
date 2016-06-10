870d85989a17ef2a585a4fb74ecb8d17aecb540c
Chenyuan Xie <1147719157xie@gmail.com>
2016-06-09 23:11:36 -0400

    Added private create method

--- Lab_3/Deck.cs
+++ Lab_3/Deck.cs
@@ -6,7 +6,70 @@ using System.Threading.Tasks;
 
 namespace Lab_3
 {
-    class Deck
+    /**
+     * <summary>
+     * This class extends the List<Card> generic type
+     * </summary>
+     * 
+     * @class Deck
+     */
+    class Deck : List<Card>
     {
+
+        // PRIVATE METHODS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        /**
+         * <summary>
+         * This method loads a list of cards with Card objects
+         * </summary>
+         * 
+         * @private
+         * @method _create
+         * @returns {void}
+         */
+        private void _create()
+        {
+            string suit = "";
+
+            for (int suitIndex = 0; suitIndex < 4; suitIndex++)
+            {
+                switch (suitIndex)
+                {
+                    case 0:
+                        suit = "spades";
+                        break;
+                    case 1:
+                        suit = "hearts";
+                        break;
+                    case 2:
+                        suit = "clubs";
+                        break;
+                    case 3:
+                        suit = "diamonds";
+                        break;
+
+                } // end suit switch
+
+                for (int face = 1; face < 14; face++)
+                {
+                    this.Add(new Card(face, suit)); // anonymous Card object to the list
+                } // end for - face
+
+            } // end for - suit
+
+        } // end CreateDeck method
+
+        // CONSTRUCTORS ++++++++++++++++++++++++++++++++++++++++++
+        /**
+         * <summary>
+         * This is the default constructor for the Deck class
+         * </summary>
+         * 
+         * @constructor Deck
+         */
+        public Deck()
+        {
+            // create my deck of cards
+            this._create();
+        }
     }
 }
