6092e5f37dc95e00cce974c84101a7d639fc59a4
Chenyuan Xie <1147719157xie@gmail.com>
2016-06-09 23:00:31 -0400

    initial commit. created card and deck class

---
+++ Lab_3/Card.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    /**
+     * <summary>
+     * This class creates an object that represents a playing card
+     * </summary>
+     * 
+     * @class Card
+     */
+    class Card
+    {
+        // PUBLIC PROPERTIES ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public int Face { get; set; }
+        public string Suit { get; set; }
+
+        /**
+         * <summary>
+         * This constructor takes two paramaters: face and suit to instantiate a Card object
+         * </summary>
+         * 
+         * @constructor Card
+         * @ param {int} face
+         * @ param {string} suit
+         */
+        public Card(int face, string suit)
+        {
+            this.Face = face;
+            this.Suit = suit;
+        }
+    }
+}
